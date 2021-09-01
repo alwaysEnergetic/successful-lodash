487e5041734d674f8ed3ad867ddb39cbfbedbf1a
heavenstar627 <kotenkoofficial@gmail.com>
2021-09-01 11:40:20 +0300

    success with bymyself , you should remember state value

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,40 +1,41 @@
 import React from "react";
 import reactDom from "react-dom";
 
-function MyFunction(props) {
-    const temp = props.temp;
-    console.log(temp)
-    if (!temp) { return ""; }
-    else if (temp > 100) {
-            return <h1> The water would boil. </h1>;
-        } else {
-            return <h1> The water would not boil. </h1>;
-        }
-    }
+function CompareTemp(props) {
+  const temp = props.celsi; 
+  console.log(temp);
+  if (temp == '') return null;
+  else if(temp > 100) {
+    return <h1> the water would boil. </h1>
+  } else {
+    return <h1> the water would not boil. </h1>
+  }
+}
+class MyFinal extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleChange = this.handleChange.bind(this);
+    this.state = { temperature: '' };
+  }
+  handleChange(e) {
+    this.setState({ temperature: e.target.value });
+  }
+  render() {
+    const temperature = this.state.temperature;
+    return(
+      <fieldset>
+        <legend> input the temperature! </legend>
+        <input
+          value = { temperature }
+          onChange = { this.handleChange } 
+          />
+        <CompareTemp celsi = { temperature } />
 
+      </fieldset>
+    ) 
+  }
 
-class MyResult extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleChange = this.handleChange.bind(this);
-        this.state = { temperature: "" };
-    }
-
-    handleChange(e) {
-        this.setState({ temperature: e.target.value });
-    }
-
-    render() {
-        const temperature = this.state.temperature;
-        return (
-            <fieldset>
-                <legend> Enter temperature: </legend>
-                <input value={temperature} onChange={this.handleChange} />
-
-                <MyFunction temp={parseFloat(temperature)} />
-            </fieldset>
-        );
-    }
 }
 
-reactDom.render(<MyResult />, document.getElementById("root"));
+
+reactDom.render(<MyFinal />, document.getElementById("root"));
