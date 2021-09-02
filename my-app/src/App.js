86d773fd65cbe74ac1e9e1e47a2bc0b1e370ae74
heavenstar627 <kotenkoofficial@gmail.com>
2021-09-02 12:38:35 +0300

    basic routing

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { Component } from 'react';
-class App extends Component {
-  render() {
-    return(
-      <div className="App">
-        <h1> Hello World! </h1>
-      </div>
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Home from "./home.js";
+import Car from "./car.js";
+import About from "./about.js";
+
+export default function App() {
+    return (
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li> <Link to="/">Home</Link> </li>
+                        <li> <Link to="/about">About</Link> </li>
+                        <li> <Link to="/car">Car</Link> </li>
+                    </ul>
+                </nav>
+                <Switch>
+                    <Route path="/about">
+                        <About />
+                    </Route>
+                    <Route path="/car">
+                        <Car />
+                    </Route>
+                    <Route path="/">
+                        <Home />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     );
-  }
 }
- 
-export default App;
+
+// function Home() {
+//     return <h2>Home</h2>;
+// }
+
+// function About() {
+//     return <h2>About</h2>;
+// }
+
+// function Users() {
+//     return <h2>Users</h2>;
+// }
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,41 +1,12 @@
-import React from "react";
-import reactDom from "react-dom";
-
-function CompareTemp(props) {
-  const temp = props.celsi; 
-  console.log(temp);
-  if (temp == '') return null;
-  else if(temp > 100) {
-    return <h1> the water would boil. </h1>
-  } else {
-    return <h1> the water would not boil. </h1>
-  }
-}
-class MyFinal extends React.Component {
-  constructor(props) {
-    super(props)
-    this.handleChange = this.handleChange.bind(this);
-    this.state = { temperature: '' };
-  }
-  handleChange(e) {
-    this.setState({ temperature: e.target.value });
-  }
-  render() {
-    const temperature = this.state.temperature;
-    return(
-      <fieldset>
-        <legend> input the temperature! </legend>
-        <input
-          value = { temperature }
-          onChange = { this.handleChange } 
-          />
-        <CompareTemp celsi = { temperature } />
-
-      </fieldset>
-    ) 
-  }
-
-}
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 
-reactDom.render(<MyFinal />, document.getElementById("root"));
